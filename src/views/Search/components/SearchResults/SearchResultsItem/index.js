10a9df60166d118affc80e8db46a6095afa869f9
juanPocampo <juanp.ocampo@hotmail.com>
2021-07-28 19:02:27 -0300

    me olvide

--- src/views/Search/components/SearchResults/SearchResultsItem/index.js
+++ src/views/Search/components/SearchResults/SearchResultsItem/index.js
@@ -1,10 +1,11 @@
 export default function SearchResultsItem({name,email}) {
     return(
-        <div style={{backgroundColor : "#E8E7E7",
-                    marginTop: "1rem",
-                    marginBotton: "1rem",
-                    padding: 10,
-                    width: "100%"}}>
+        <div 
+        style={{backgroundColor : "#E8E7E7",
+        marginTop: "1rem",
+        marginBotton: "1rem",
+        padding: 10,
+        width: "100%"}}>
             <p>{name}</p>
             <p>{email}</p>
         </div>
